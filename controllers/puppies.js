34b4de7e2e36a4ba45ba58ab9f98ac2e01ce323b
geedtd <grcazares@gmail.com>
2022-09-09 22:00:06 -0700

    finishing up delete functionality and going to install CORS dependency

--- controllers/puppies.js
+++ controllers/puppies.js
@@ -3,8 +3,53 @@ import { Puppy } from "../models/puppy.js";
 
 export {
     create,
+    index,
+    show,
+    update,
+    deletePuppy as delete,
 }
 
 function create(req, res) {
-    console.log('this create works')
+    Puppy.create(req.body)
+    .then(puppy => res.status(201).json(puppy))
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json(err)
+    })
+}
+
+function index(req, res) {
+    Puppy.find({})
+    .then(puppies => res.status(200).json(puppies))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+}
+
+function show(req, res) {
+    Puppy.findById(req.params.id)
+    .then(puppy => res.status(200).json(puppy))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+}
+
+function update(req, res) {
+    Puppy.findByIdAndUpdate(req.params.id, req.body, {new: true} )
+    .then(puppy => res.status(204).json(puppy))
+    .catch( err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+}
+
+function deletePuppy(req, res) {
+    Puppy.findByIdAndDelete(req.params.id)
+    .then(puppy => res.status(204).json(puppy))
+    .catch( err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
 }
--- routes/puppies.js
+++ routes/puppies.js
@@ -5,6 +5,13 @@ const router = Router()
 
 router.post('/', puppiesCtrl.create)
 
+router.get('/', puppiesCtrl.index)
+
+router.get('/:id', puppiesCtrl.show)
+
+router.put('/:id', puppiesCtrl.update)
+
+router.delete('/:id', puppiesCtrl.delete)
 export {
   router
 }
